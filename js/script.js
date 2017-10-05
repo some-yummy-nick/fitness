ac886d4025df153d4a07e7baa223d546e8db6cc9
Marat Shaymardanov <yeffasol@mail.ru>
2017-10-05 20:05:34 +0300

    Add form

--- js/script.js
+++ js/script.js
@@ -9,4 +9,38 @@ $(document).ready(function(){
     })
 
   })();
+
+  (function() {
+	  $('.variable-width').slick({
+		  arrows: true,
+		  dots: false,
+		  infinite: true,
+		  speed: 300,
+		  slidesToShow: 3,
+		  centerMode: false,
+		  variableWidth: true,
+		  responsive: [{
+			  breakpoint: 1024,
+			  settings: {
+				  slidesToShow: 3,
+				  slidesToScroll: 3,
+				  infinite: true,
+				  dots: false
+			  }
+		  }, {
+			  breakpoint: 600,
+			  settings: {
+				  slidesToShow: 2,
+				  slidesToScroll: 2
+			  }
+		  }, {
+			  breakpoint: 480,
+			  settings: {
+				  slidesToShow: 1,
+				  slidesToScroll: 1
+			  }
+		  }]
+	  });
+  })();
 });
+
